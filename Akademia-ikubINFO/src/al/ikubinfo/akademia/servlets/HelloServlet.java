5ecbadc14fb4a3aa100d03e34746cf5d4044e851
Endrik Lelo <endrik.lelo@ikubinfo.al>
2018-05-10 15:09:21 +0200

    First commit. Created sample project

---
+++ Akademia-ikubINFO/src/al/ikubinfo/akademia/servlets/HelloServlet.java
@@ -0,0 +1,49 @@
+package al.ikubinfo.akademia.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+    }
+    
+    
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("INIT - S1");
+		super.init();
+	}
+
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("TEST");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
